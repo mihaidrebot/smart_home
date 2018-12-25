ac2a4cffb1b7b50bec111dbeb8ecbafbe00f0c3f
mihaidrebot <mihai.voicu.drebot@gmail.com>
2018-12-25 14:07:57 +0200

    test

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Acum merge mai bine!\n');
+  res.send(' hgfhgfyhg fh hgfh \n');
 });
 
 app.listen(PORT, HOST);
