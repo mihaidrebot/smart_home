c1f79302af66a2d0a150ba0084b70534fea35dc1
mihaidrebot <mihai.voicu.drebot@gmail.com>
2018-12-25 23:46:36 +0200

    added frontend

--- server.js
+++ server.js
@@ -4,13 +4,30 @@ const express = require('express');
 
 // Constants
 const PORT = 8080;
-const HOST = '0.0.0.0';
+const HOST = 'localhost';
 
 // App
 const app = express();
-app.get('/', (req, res) => {
-  res.send(' hgfhgfyhg fh hgfh \n');
+// app.get('/', (req, res) => {
+//   res.send('Testing \n');
+// });
+
+app.get('*', function(req, res) {
+  res.sendfile('./frontend/src/index.html'); // load the single view file (angular will handle the page changes on the front-end)
 });
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+app.listen(PORT, HOST,()=>{
+  console.log(`Running on http://${HOST}:${PORT}`);
+});
+
+
+app.route('/api/cats').get((req, res) => {
+  res.send({
+    cats: [{ name: 'lilly' }, { name: 'lucy' }]
+  });
+});
+
+app.route('/api/cats/:name').get((req, res) => {
+  const requestedCatName = req.params['name'];
+  res.send({ name: requestedCatName });
+});
